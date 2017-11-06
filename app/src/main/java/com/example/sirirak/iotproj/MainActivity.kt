1becbac4b2b24eee1f9e36310ea13b6f55600c9d
sirirak <hotmail22866@gmail.com>
2017-11-07 01:21:49 +0700

    Firebase Authentication with Email and password

--- app/src/main/java/com/example/sirirak/iotproj/MainActivity.kt
+++ app/src/main/java/com/example/sirirak/iotproj/MainActivity.kt
@@ -2,11 +2,80 @@ package com.example.sirirak.iotproj
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import kotlinx.android.synthetic.main.activity_main.*
+import android.R.attr.password
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
+    private var email: String? = null
+    private var password: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                // User is signed in
+            } else {
+                // User is signed out
+            }
+            // ...
+        }
+    }
+
+    fun onClickCreate(view:View){
+        email = edt_email.text.toString()
+        password = edt_password.text.toString()
+
+        mAuth!!.createUserWithEmailAndPassword(email!!, password!!)
+                .addOnCompleteListener(this) { task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                    // ...
+                }
+    }
+    fun onClickSignIn(view: View){
+        email = edt_email.text.toString()
+        password = edt_password.text.toString()
+
+        mAuth!!.signInWithEmailAndPassword(email!!, password!!)
+                .addOnCompleteListener(this) { task ->
+                    if (!task.isSuccessful) {
+
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                    // ...
+                }
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        mAuth!!.addAuthStateListener(mAuthListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mAuthListener != null) {
+            mAuth!!.removeAuthStateListener(mAuthListener!!);
+        }
     }
 }
